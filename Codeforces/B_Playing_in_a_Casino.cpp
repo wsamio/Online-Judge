6b3f46fbcf60f2a6fa5cc993d95a0d6edb72e499
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-19 03:56:56 +0600

    a

--- Codeforces/B_Playing_in_a_Casino.cpp
+++ Codeforces/B_Playing_in_a_Casino.cpp
@@ -6,30 +6,28 @@ using namespace std;
 void Testcase() {
   int n, m;
   cin >> n >> m;
-  vector<vector<ll>> a(n + 1, vector<ll> (m));
-  for(int i = 1; i <= n; i++) {
-    for(int j = 0; j < m; j++) {
+  vector<vector<ll>> a(m, vector<ll> (n + 1, 0));
+  for(int j = 1; j <= n; j++) {
+    for(int i = 0; i < m; i++) {
       cin >> a[i][j];
     }
   }
   ll ans = 0;
-  for(int j = 0; j < m; j++) {
-    vector<ll> tmp(n + 1);
-    for(int i = 1; i <= n; i++) {
-      tmp[i] = a[i][j];
+  for(int i = 0; i < m; i++) {
+    sort(a[i].begin(), a[i].end());
+    for(int j = 1; j <= n; j++) {
+      a[i][j] += a[i][j - 1];
     }
-    sort(tmp.begin(), tmp.end());
-    for(int i = 1; i <= n; i++) {
-      tmp[i] += tmp[i - 1];
-    }
-    for(int i = 1; i <= n; i++) {
-      ans += (tmp[n] - tmp[i - 1]) - (tmp[i] - tmp[i - 1]) * (n + 1 - i);
+    for(int j = 1; j <= n; j++) {
+      ans += a[i][n] - a[i][j - 1] - (a[i][j] - a[i][j - 1]) * (n - j + 1);
     }
   }
   cout << ans << '\n';
 }
 
 int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
   int T;
   cin >> T;
   while(T--){
