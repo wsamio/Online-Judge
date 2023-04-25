a78db1e33cd7bf880b912921120e99979d74eb2d
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-25 07:39:41 +0600

    a

---
+++ Codeforces/C_Traffic_Light.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+void Testcase() {
+  int n;
+  char cur;
+  string s;
+  cin >> n >> cur >> s;
+  s += s;
+  int d = 0;
+  for(int i = 0, j; i < 2*n; i++) if(s[i] == cur) {
+      j = i;
+      while(i < 2*n and s[i] != 'g') i++;
+      d = max(d, i - j);
+    }
+  cout << d << '\n';
+}
+
+int main() {
+  int T;
+  cin >> T;
+  while(T--) {
+    Testcase();
+  }
+}
