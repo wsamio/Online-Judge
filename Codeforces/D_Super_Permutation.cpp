c77aa5d70700f540c4d26c11998c7f4c3b667d6d
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-26 16:57:59 +0600

    a

---
+++ Codeforces/D_Super_Permutation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Testcase() {
+  int n;
+  cin >> n;
+  if(n == 1) {
+    cout << "1\n"; return;
+  }
+  if(n&1) {
+    cout << "-1\n"; return;
+  }
+  vector<int> a(n+1);
+  for(int i = 0; i < n; i++) {
+    if(i&1) cout << i;
+    else cout << n - i;
+    cout << (i == n - 1 ? '\n' : ' ');
+  }
+}
+
+int main() {
+  int T;
+  cin >> T;
+  while(T--) {
+    Testcase();
+  }
+}
